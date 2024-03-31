9898bb638eadf96b668bec7b735e53627a862f70
conghiep1997 <147464182+conghiep1997@users.noreply.github.com>
2024-03-31 19:49:51 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
-const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
+mconst textConfig = {
+  text1: "He luu Elm!",
+  text2: "Anh có điều này muốn hỏi Elm nhớ phải trả lời thật lòng nhaaa.",
+  text3: "Elm thích anh có phải không nào ._.",
+  text4: "Nếu Elm ko trả lời mà thoát ra tức là muốn làm vợ anh rùi đó nha :v",
+  text5: "Anh mơ à???",
   text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text7: "lí do Elm thích anh đi :vvvv",
+  text8: "Gửi cho Anh <3",
+  text9: "Vì anh đẹp try vlllll",
+  text10: "Anh biết mà ^^ Yêu Elm 3000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Tối nay Anh qua đón Elm đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho Anh đi nàooo",
   text12: "Okii lunn <3",
 };
 
