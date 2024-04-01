d200a072d1f552af1b6b340999d3994acc143d50
conghiep1997 <147464182+conghiep1997@users.noreply.github.com>
2024-04-01 18:53:34 +0700

    Update index.html

--- js/myJs.js
+++ js/myJs.js
@@ -1,13 +1,13 @@
-mconst textConfig = {
-  text1: "He luu Elm!",
+const textConfig = {
+  text1: "Hế luu Elm!",
   text2: "Anh có điều này muốn hỏi Elm nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Elm thích anh có phải không nào ._.",
-  text4: "Nếu Elm ko trả lời mà thoát ra tức là muốn làm vợ anh rùi đó nha :v",
+  text3: "Elm làm ngiu anh nhaaa ._.",
+  text4: "Nếu Elm ko trả lời mà thoát ra tức là muốn làm ngiu anh rùi đó nha :v",
   text5: "Anh mơ à???",
   text6: "Yêu ơi là yêu <3",
   text7: "lí do Elm thích anh đi :vvvv",
   text8: "Gửi cho Anh <3",
-  text9: "Vì anh đẹp try vlllll",
+  text9: "Vì anh đẹp try nhứt",
   text10: "Anh biết mà ^^ Yêu Elm 3000",
   text11:
     "Tối nay Anh qua đón Elm đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho Anh đi nàooo",
@@ -28,6 +28,8 @@ $(document).ready(function () {
   $("#text3").html(textConfig.text3);
   $("#text4").html(textConfig.text4);
   $("#no").html(textConfig.text5);
+  $("#no1").html(textConfig.text5);
+  $("#no2").html(textConfig.text5);
   $("#yes").html(textConfig.text6);
 
   function firstQuestion() {
@@ -62,24 +64,176 @@ $(document).ready(function () {
   function moveButton() {
     var audio = new Audio("sound/Swish1.mp3");
     audio.play();
-    if (screen.width <= 600) {
-      var x = Math.random() * 300;
-      var y = Math.random() * 500;
-    } else {
-      var x = Math.random() * 500;
-      var y = Math.random() * 500;
-    }
+    let x = xxx();
+    let y = yyy();
     var left = x + "px";
     var top = y + "px";
     $("#no").css("left", left);
     $("#no").css("top", top);
   }
+  // move random button póition
+  function moveButton1() {
+    var audio = new Audio("sound/Swish1.mp3");
+    audio.play();
+    let x = xxx();
+    let y = yyy();
+    var left = x + "px";
+    var top = y + "px";
+    $("#no1").css("left", left);
+    $("#no1").css("top", top);
+  }
+  // move random button póition
+  function moveButton2() {
+    var audio = new Audio("sound/Swish1.mp3");
+    audio.play();
+    let x = xxx();
+    let y = yyy();
+    var left = x + "px";
+    var top = y + "px";
+    $("#no2").css("left", left);
+    $("#no2").css("top", top);
+  }
+  // move random button póition
+  function moveButton3() {
+    var audio = new Audio("sound/Swish1.mp3");
+    audio.play();
+    let x = xxx();
+    let y = yyy();
+    var left = x + "px";
+    var top = y + "px";
+    $("#no3").css("left", left);
+    $("#no3").css("top", top);
+  }
+  // move random button póition
+  function moveButton4() {
+    var audio = new Audio("sound/Swish1.mp3");
+    audio.play();
+    let x = xxx();
+    let y = yyy();
+    var left = x + "px";
+    var top = y + "px";
+    $("#no4").css("left", left);
+    $("#no4").css("top", top);
+  }
+  // move random button póition
+  function moveButton5() {
+    var audio = new Audio("sound/Swish1.mp3");
+    audio.play();
+    let x = xxx();
+    let y = yyy();
+    var left = x + "px";
+    var top = y + "px";
+    $("#no5").css("left", left);
+    $("#no5").css("top", top);
+  }
+  // move random button póition
+  function moveButton6() {
+    var audio = new Audio("sound/Swish1.mp3");
+    audio.play();
+    let x = xxx();
+    let y = yyy();
+    var left = x + "px";
+    var top = y + "px";
+    $("#no6").css("left", left);
+    $("#no6").css("top", top);
+  }
+  // move random button póition
+  function moveButton7() {
+    var audio = new Audio("sound/Swish1.mp3");
+    audio.play();
+    let x = xxx();
+    let y = yyy();
+    var left = x + "px";
+    var top = y + "px";
+    $("#no7").css("left", left);
+    $("#no7").css("top", top);
+  }
+  // move random button póition
+  function moveButton8() {
+    var audio = new Audio("sound/Swish1.mp3");
+    audio.play();
+    let x = xxx();
+    let y = yyy();
+    var left = x + "px";
+    var top = y + "px";
+    $("#no8").css("left", left);
+    $("#no8").css("top", top);
+  }
+  // move random button póition
+  function moveButton9() {
+    var audio = new Audio("sound/Swish1.mp3");
+    audio.play();
+    let x = xxx();
+    let y = yyy();
+    var left = x + "px";
+    var top = y + "px";
+    $("#no9").css("left", left);
+    $("#no9").css("top", top);
+  }
+  // move random button póition
+  function moveButton10() {
+    var audio = new Audio("sound/Swish1.mp3");
+    audio.play();
+    let x = xxx();
+    let y = yyy();
+    var left = x + "px";
+    var top = y + "px";
+    $("#no10").css("left", left);
+    $("#no10").css("top", top);
+  }
+  
+  function xxx() {
+    let x = 0;
+    if (screen.width <= 600) {
+      x = Math.random() * 300;
+    } else {
+      x = Math.random() * 900;
+    }
+    return x;
+  }
+  
+  function yyy() {
+    let y = 0;
+    if (screen.width <= 600) {
+      y = Math.random() * 500;
+    } else {
+      y = Math.random() * 900;
+    }
+    return y;
+  }
 
-  var n = 0;
   $("#no").mousemove(function () {
-    if (n < 1) switchButton();
-    if (n > 1) moveButton();
-    n++;
+    moveButton();
+  });
+  $("#no1").mousemove(function () {
+    moveButton1();
+  });
+  $("#no2").mousemove(function () {
+    moveButton2();
+  });
+  $("#no3").mousemove(function () {
+    moveButton3();
+  });
+  $("#no4").mousemove(function () {
+    moveButton4();
+  });
+  $("#no5").mousemove(function () {
+    moveButton5();
+  });
+  $("#no6").mousemove(function () {
+    moveButton6();
+  });
+  $("#no7").mousemove(function () {
+    moveButton7();
+  });
+  $("#no8").mousemove(function () {
+    moveButton8();
+  });
+  $("#no9").mousemove(function () {
+    moveButton9();
+  });
+  $("#no10").mousemove(function () {
+    moveButton10();
   });
   $("#no").click(() => {
     if (screen.width >= 900) switchButton();
@@ -138,7 +292,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "http://fb.com";
+            window.location = "https://www.youtube.com/watch?v=M4zX6mPCHJI";
           },
         });
       }
